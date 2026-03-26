a5bb7730bbc4fd01ed2ea81bf986541db80e9f9b
Vijay Dudhal <vijaydudhal2019@gmail.com>
2026-03-26 22:37:03 +0530

    Initial setup

---
+++ src/main/java/com/vijaydev_auth/auth_app_backend/AuthAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vijaydev_auth.auth_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthAppBackendApplication.class, args);
+	}
+
+}
